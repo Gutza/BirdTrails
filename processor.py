8b4abea5f867d8fa1867b19f05742c7d04494d56
Bogdan Stăncescu <gutza@moongate.ro>
2022-10-27 22:00:06 +0300

    Better processor, but poor timing

--- processor.py
+++ processor.py
@@ -13,21 +13,29 @@ lo_val = 25
 hi_val = 50
 
 dark_im: np.array = None
+idx = 0
 first = True
-for idx, filename in tqdm(enumerate(os.listdir(in_folder)), desc="Processing images", total=im_count):
+for filename in tqdm(os.listdir(in_folder), desc="Processing images", total=im_count):
     filepath = os.path.join(in_folder, filename)
     im = cv.cvtColor(cv.imread(filepath), cv.COLOR_BGR2GRAY)
     
     im = np.minimum(im, hi_val)
     im = np.maximum(im, lo_val)
+    im = (im - lo_val) * (256. - hi_val)
 
     if first:
         dark_im = im
         first = False
-    else:
-        dark_im = np.minimum(dark_im, im)
+        continue
 
-    image_path_out = os.path.join(out_folder, filename)
+    dark_im = np.minimum(im, dark_im)
+
+    if np.array_equal(im, dark_im):
+        continue
+
+    idx += 1
+
+    image_path_out = os.path.join(out_folder, f"{filename}-{idx}.jpg")
     cv.imwrite(image_path_out, dark_im)
 
     if idx % 10:
@@ -35,6 +43,7 @@ for idx, filename in tqdm(enumerate(os.listdir(in_folder)), desc="Processing ima
 
     cv.imshow("Trails", dark_im)
     if 27 == cv.waitKey(2):
+        print(f"Aborting the process at {filename}")
         exit()
 
 print("Finished")
