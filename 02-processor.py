1151845c157eff8392c529c2e2e467851cff042f
Bogdan Stăncescu <gutza@moongate.ro>
2022-10-28 19:56:53 +0300

    Multi-threaded, in-memory processing

--- 02-processor.py
+++ 02-processor.py
@@ -1,67 +1,93 @@
 import math
+from typing import List
 import numpy as np
 import cv2 as cv
 import os
 from tqdm import tqdm
+from threading import Thread, Lock
+import time
 
 in_folder = "../R0008026-preprocessed"
-temp_folder = "../R0008026-temp"
 out_folder = "../R0008026-out"
+
+latency = 750
+duration = 100
 
 image_list = os.listdir(in_folder)
 image_list.sort()
 
-dark_im: np.array = None
-idx = 0
-first = True
-prev_im: np.array = None
+log_mutex = Lock()
 
-latency = 10
-duration = 100
+tmp = cv.imread(os.path.join(in_folder, image_list[0]))
+print("Allocating the history array... ", end="", flush=True)
+history: np.array = 255 + np.zeros((latency + duration, tmp.shape[0], tmp.shape[1]))
+print("done.")
 
-cache_size = 25
+def shIdx(n: int) -> int:
+    """Computes a safe history index for a given value. Simply applies modulo.
 
-for idx, filename in tqdm(enumerate(image_list), total=len(image_list), desc="Processing images", disable=True):
-    im = cv.imread(os.path.join(in_folder, filename))[:, :, 0]
-    cv.imwrite(os.path.join(temp_folder, filename), im)
+    Args:
+        n (int): The desired index (linear)
 
-    if first:
-        first = False
-        continue
+    Returns:
+        int: The correct index (circular)
+    """
+    return (n + len(history)) % len(history)
 
-    final_im = None
+def process_history_layer(lIdx: int, trY: float):
+    translation_matrix = np.eye(3)
+    translation_matrix[1, 2] = trY
 
-    minI = max(0, idx-latency-duration)
-    for i in range(idx, minI - 1, -1):
-        if i < idx - latency:
-            trY = -(idx - latency - i)/10.
-            translation_matrix = np.eye(3)
-            translation_matrix[1, 2] = trY
-            old_imagepath = os.path.join(temp_folder, image_list[i])
-            old_im = np.minimum(255., cv.imread(old_imagepath)[:, :, 0] - trY/10.)
-            old_im = cv.GaussianBlur(old_im, (3, 3), 0)
-            old_im = cv.warpPerspective(old_im, translation_matrix, (old_im.shape[1], old_im.shape[0])) # Actually just translation
-            old_im[math.floor(trY):, :] = 255
-            cv.imwrite(old_imagepath, old_im)
-            if final_im is None:
-                final_im = old_im
+    log_mutex.acquire()
+    old_im = history[lIdx, :, :]
+    log_mutex.release()
+
+    if np.min(old_im) == 255:
+        return
+
+    old_im = np.minimum(255., old_im - trY/10.)
+    old_im = cv.GaussianBlur(old_im, (7, 3), 0)
+    old_im = cv.warpPerspective(old_im, translation_matrix, (old_im.shape[1], old_im.shape[0])) # Actually just translation
+    old_im[math.floor(trY):, :] = 255 # Fill with white
+
+    log_mutex.acquire()
+    history[lIdx, :, :] = old_im
+    log_mutex.release()
+
+threads: List[Thread] = []
+max_thread_count = 12
+def herd_thread(new_thread: Thread):
+    global threads
+    go_time = len(threads) < max_thread_count
+    while not go_time:
+        live_threads = []
+        for t in threads:
+            if t.is_alive():
+                live_threads.append(t)
             else:
-                final_im = np.minimum(final_im, old_im)
-        elif final_im is None:
-            final_im = cv.imread(os.path.join(temp_folder, image_list[i]))[:, :, 0]
-        else:
-            final_im = np.minimum(final_im, cv.imread(os.path.join(temp_folder, image_list[i]))[:, :, 0])
+                t.join()
+                go_time = True
+        threads = live_threads
+        if not go_time:
+            time.sleep(0.001) # Wait a millisecond there, mister!
 
+    new_thread.start()
+    threads.append(new_thread)
+
+for idx, filename in tqdm(enumerate(image_list), total=len(image_list), desc="Processing images", disable=False):
+    hIdx = shIdx(idx)
+    history[hIdx, :, :] = cv.imread(os.path.join(in_folder, filename))[:, :, 0]
+
+    minI = max(0, idx - latency - duration + 1)
+    maxI = max(0, idx - latency)
+    for i in range(minI, maxI):
+        herd_thread(Thread(target = process_history_layer, args = [shIdx(i), -(idx - latency - i)/10.]))
+
+    for t in threads:
+        t.join()
+    threads = []
+
+    final_im = np.min(history, 0)
     cv.imwrite(os.path.join(out_folder, f"trails-{idx:05d}.png"), final_im)
 
-    if idx % 10:
-        continue
-
-    cv.imshow("Trails", final_im)
-    if 27 == cv.waitKey(2):
-        print(f"Aborting the process at {filename}")
-        exit()
-
-print("Finished")
-cv.imshow("Trails", dark_im)
-cv.waitKey()
+print("Finished.")
